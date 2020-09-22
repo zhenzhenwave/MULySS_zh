af8ce5e7a73ca8a1319383e45c33c37d79a240b0
zhenzhenwave <841409819@qq.com>
2020-09-22 10:23:13 +0800

    add readme

---
+++ README.m
@@ -0,0 +1,15 @@
+# TODO
+- [ ] 学习使用程序，实验M矮星的拟合
+- [ ] 筛选数据、交叉验证
+- [ ] 针对M矮星，选择数据库，改进拟合参数
+
+# ULySS
++ ULySS是法国的里昂大学基于IDL\[GDL](https://github.com/gnudatalanguage/gdl)开发的光谱分析软件。其线性组合了一些非线性函数与视向速度成分的卷积（并乘以多项式）来拟合光谱。能用于恒星大气参数和星族形成历史的研究。
++ 使用手册：[官网](http://ulyss.univ-lyon1.fr/)、[我的笔记](https://github.com/zhenzhenwave/ULySS_M/blob/master/ULySS_note.md)
+
+# ULySS_M
++ ULySS对于M矮星的拟合仍有可以改善的工作。
++ 我的[工作记录](https://github.com/zhenzhenwave/ULySS_M/blob/master/Project_Log.md)
+
+# Acknowledgements
+ULySS是一个开源项目，但因为工作需要，本项目暂时不开源。
